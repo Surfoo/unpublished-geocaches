7fe7f22f433ec52eee07d106db2c41ba0e8fd326
Surfoo <surfooo@gmail.com>
2017-03-17 22:31:39 +0100

    Fixed regex

--- web/unpublished.php
+++ web/unpublished.php
@@ -25,7 +25,7 @@ try {
 
 $htmlResponse = (string) $response->getBody();
 
-if(!preg_match_all('#<li>\s*<img src="[https?://www.geocaching.com]?/images/wpttypes/sm/[a-z0-9]+.gif" width="16" height="16" alt="" />' .
+if(!preg_match_all('#<li>\s*<img src="[^"]+" width="16" height="16" alt="" />' .
                   '\s*<a href="https?://www.geocaching.com/seek/cache_details.aspx\?guid=(.*)">(.*)</a>[\s+]?</li>#msU', $htmlResponse, $elements)) {
     renderAjax(array('success' => false, 'message' => 'No unpublished caches found.'));
 }
